6e79a7c5369310d1e064373b2ce6064f82cdb5a8
Tarak Shah <tarak.shah90@gmail.com>
2019-12-30 03:35:37 +0530

    textblob and vader sentiment

--- tweepy_credentials.py
+++ tweepy_credentials.py
@@ -3,8 +3,3 @@
 # access_token = "XXXXX-XXXXX"
 # access_secret = "XXXXX"
 
-
-consumer_token = "Zctc9kTjHni0oSBrv1lbHaoKa"
-consumer_secret = "rjCkGEpXrXt3NtlJWNRsKHpvjVtSiGPcCvIhxseUyGEZhsRiPL"
-access_token = "423194073-ExnjpOQxBqOQUumXz4lZMupb1rrSeTmmvx6VlkAX"
-access_secret = "fJn6ufo7InRwA3B0yK3ytXP8ZgdNI2TQEVEgMm1kQF9pn"
