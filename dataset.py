616fe7fee02ec446a9f6d5a0e01d1fe77583fa07
Tarak Shah <tarak.shah90@gmail.com>
2019-12-30 03:35:05 +0530

    textblob and vader sentiment

--- dataset.py
+++ dataset.py
@@ -8,15 +8,15 @@ import pandas as pd
 import json
 import time
 
-auth = tweepy.OAuthHandler(tweepy_credentials.consumer_token,tweepy_credentials.consumer_secret)
-
-auth.set_access_token(tweepy_credentials.access_token, tweepy_credentials.access_secret)
-
-api = tweepy.API(auth)
-
-query = 'Flipkart'
-
-max_id = None
+# auth = tweepy.OAuthHandler(tweepy_credentials.consumer_token,tweepy_credentials.consumer_secret)
+#
+# auth.set_access_token(tweepy_credentials.access_token, tweepy_credentials.access_secret)
+#
+# api = tweepy.API(auth)
+#
+# query = 'microsoft'
+#
+# max_id = None
 
 # tweets_raw = []
 #
@@ -75,84 +75,118 @@ max_id = None
 ################################################ TEST 1 ##########################################
 
 
-# class TwitterClient(object):
-#     def __init__(self):
-#         # Access Credentials
-#         consumer_key = tweepy_credentials.consumer_token
-#         consumer_secret = tweepy_credentials.consumer_secret
-#         access_token = tweepy_credentials.access_token
-#         access_token_secret = tweepy_credentials.access_secret
-#
-#
-#         try:
-#             # OAuthHandler
-#             authorization = OAuthHandler(consumer_key, consumer_secret)
-#             # setting access token and secret
-#             authorization.set_access_token(access_token, access_token_secret)
-#             # create tweepy API object to fetch tweets
-#             self.api = tweepy.API(authorization, wait_on_rate_limit=True)
-#
-#         except tweepy.TweepError as e:
-#             print(f"Error: Twitter Authentication Failed - \n{str(e)}")
-#
-#             # Function to fetch tweets
-#
-#
-#     def get_tweets(self, query, maxTweets=2000):
-#         # empty list to store parsed tweets
-#         tweets = []
-#         sinceId = None
-#         tweet_id = -1
-#         tweetCount = 0
-#         tweetsPerQry = 180
-#
-#         while tweetCount < maxTweets:
-#             try:
-#                 if (tweet_id <= 0):
-#                     if (not sinceId):
-#                         new_tweets = self.api.search(q=query, count=tweetsPerQry)
-#                     else:
-#                         new_tweets = self.api.search(q=query, count=tweetsPerQry,
-#                                                      since_id=sinceId)
-#                 else:
-#                     if (not sinceId):
-#                         new_tweets = self.api.search(q=query, count=tweetsPerQry,
-#                                                      max_id=str(tweet_id - 1))
-#                     else:
-#                         new_tweets = self.api.search(q=query, count=tweetsPerQry,
-#                                                      max_id=str(tweet_id - 1),
-#                                                      since_id=sinceId)
-#                 if not new_tweets:
-#                     print("No more tweets found")
-#                     break
-#
-#                 for tweet in new_tweets:
-#                     parsed_tweet = {}
-#                     parsed_tweet['tweets'] = tweet.text
-#
-#                     # appending parsed tweet to tweets list
-#                     if tweet.retweet_count > 0:
-#                         # if tweet has retweets, ensure that it is appended only once
-#                         if parsed_tweet not in tweets:
-#                             tweets.append(parsed_tweet)
-#                     else:
-#                         tweets.append(parsed_tweet)
-#
-#                 tweetCount += len(new_tweets)
-#                 print("Downloaded {0} tweets".format(tweetCount))
-#                 max_id = new_tweets[-1].id
-#
-#             except tweepy.TweepError as e:
-#                 print("Tweepy error : " + str(e))
-#                 break
-#
-#         # return pd.DataFrame(tweets).to_csv('tweets_flipkart.csv', encoding='utf-8')
-#
-#         return tweets
-#
-# tweetsHandler = TwitterClient()
-# tweetsHandler.get_tweets('Flipkart')
+class TwitterClient(object):
+    def __init__(self):
+
+        try:
+            # Authenticate
+            authorization = OAuthHandler(tweepy_credentials.consumer_token, tweepy_credentials.consumer_secret)
+            # setting access token and secret
+            authorization.set_access_token(tweepy_credentials.access_token, tweepy_credentials.access_secret)
+            # create tweepy API object to fetch tweets
+            self.api = tweepy.API(authorization, wait_on_rate_limit=True)
+
+        except tweepy.TweepError as e:
+            print(f"Error: Twitter Authentication Failed - \n{str(e)}")
+
+
+    def fetch_tweets(self, search, max_Tweets=2100):
+        # empty list to store parsed tweets
+        tweets = []
+        last_tweetId = None
+        tweet_id = -1
+        tweetCount = 0
+        tweetsPerQry = 100
+
+        while tweetCount < max_Tweets:
+            try:
+                if (tweet_id <= 0):
+                    if (not last_tweetId):
+                        new_tweet = self.api.search(q=search, count=tweetsPerQry)
+                    else:
+                        new_tweet = self.api.search(q=search, count=tweetsPerQry,
+                                                     since_id=last_tweetId)
+                else:
+                    if (not last_tweetId):
+                        new_tweet = self.api.search(q=search, count=tweetsPerQry,
+                                                     max_id =str(tweet_id - 1))
+                    else:
+                        new_tweet = self.api.search(q=search, count=tweetsPerQry,
+                                                     max_id =str(tweet_id - 1),
+                                                     since_id =last_tweetId)
+                if not new_tweet:
+                    print("New tweets Not found")
+                    break
+
+                for tweet in new_tweet:
+                    parsed_tweet = {}
+                    parsed_tweet['tweet'] = tweet.text
+
+                    # append new tweets to a list
+                    if tweet.retweet_count > 0:
+                        # check if it's a retweet and append only once if so...
+                        if parsed_tweet not in tweets:
+                            tweets.append(parsed_tweet)
+                    else:
+                        tweets.append(parsed_tweet)
+
+                tweetCount += len(new_tweet)
+                print("Downloaded "+ str(tweetCount) +  " tweets")
+                tweet_id = new_tweet[-1].id
+
+            except tweepy.TweepError as e:
+                print("Tweepy error : " + str(e))
+                break
+
+        return pd.DataFrame(tweets).to_csv('tweets_flipkart.csv', encoding='utf-8', index=False)
+
+tweetsHandler = TwitterClient()
+tweetsHandler.fetch_tweets('Flipkart')
 
 ##############################################################################################################
 
 
+
+########################### Selenium #####################################################################
+
+# from selenium import webdriver
+# from selenium.webdriver.common.keys import Keys
+# class SeleniumClient(object):
+#     def __init__(self):
+#         #Initialization method.
+#         self.chrome_options = webdriver.ChromeOptions()
+#         self.chrome_options.add_argument('--headless')
+#         self.chrome_options.add_argument('--no-sandbox')
+#         self.chrome_options.add_argument('--disable-setuid-sandbox')
+#         options = self.chrome_options
+#         # you need to provide the path of chromdriver in your system
+#         self.browser = webdriver.Chrome('C:/Chromedriver/chromedriver')
+#
+#         self.base_url = 'https://twitter.com/search?q='
+#
+#     def get_tweets(self, query):
+#         '''
+#         Function to fetch tweets.
+#         '''
+#         try:
+#             self.browser.get(self.base_url+query)
+#             time.sleep(2)
+#
+#             body = self.browser.find_element_by_tag_name('body')
+#
+#             for _ in range(30):
+#                 body.send_keys(Keys.PAGE_DOWN)
+#                 time.sleep(0.3)
+#
+#             timeline = self.browser.find_element_by_id('timeline')
+#             tweet_nodes = timeline.find_elements_by_css_selector('.tweet-text')
+#
+#             self.browser.quit()
+#             return pd.DataFrame({'tweets': [tweet_node.text for tweet_node in tweet_nodes]})
+#
+#         except:
+#             print("Selenium - An error occured while fetching tweets.")
+#
+# selenium_client = SeleniumClient()
+#
+# tweets_df = selenium_client.get_tweets('AI and Deep learning')
--- sentiments.py
+++ sentiments.py
@@ -1,36 +1,127 @@
 import pandas as pd
 import nltk
+import csv
 from nltk import tokenize, word_tokenize, util
-from nltk.corpus import stopwords
 from nltk.stem.snowball import SnowballStemmer
+from nltk.corpus import stopwords
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
+import re
+snowballStemmer = SnowballStemmer("english")
+stopwords = stopwords.words('english')
 import vaderSentiment
+from textblob import TextBlob
 
-file = pd.read_csv('flipkart_tweets.csv', index_col = "tweets")
+file = pd.read_csv('tweets_flipkart.csv')
+print(file.count())
 # print(file['tweets'])
 
 
 
 # pre-processing
-import re
 
-snowballStemmer = SnowballStemmer("english")
-stopwords = stopwords.words('english')
+def processing_text(tweet):
+    tweet = re.sub(r'@[A-Za-z0-9_]+', '', tweet)
+    tweet = re.sub(r"http\S+", "", tweet)
+    tweet = re.sub(r"[0-9]*", "", tweet)
+    tweet = re.sub(r"&amp", "", tweet)
+    tweet = tweet.lower()
+
+    clean_tweet = tweet.lower()
+    return clean_tweet
 
 
-clean_tweets = []
-def processing_text(tweets):
+def tokens_stemmer(tweets):
 
-    for tweet in tweets:
-        tweet = re.sub(r'@[A-Za-z0-9_]+', '', tweet)
-        tweet = re.sub(r"http\S+", "", tweet)
-        tweet = re.sub(r"[0-9]*", "", tweet)
-        tweet = re.sub(r"&amp", "", tweet)
-        tweet = tweet.lower()
+    total_tweets_tokens = []
+    total_stemmer_tokens = []
 
-        clean_tweets.append(tweet)
-        return clean_tweets
 
-processing_text(file)
+    tokens = nltk.word_tokenize(tweets)
 
-print(clean_tweets)
+    tweets_tokens = []
+    snowballStemmer_tokens = []
+
+    for token in tokens:
+        if token not in stopwords:
+            tweets_tokens.append(token)
+            snowballStemmer_tokens.append(snowballStemmer.stem(token))
+    total_tweets_tokens.append(tweets_tokens)
+    total_stemmer_tokens.append(snowballStemmer_tokens)
+
+    tweet_token_total = []
+    tweet_stem_total = []
+
+    # Convert stemming and tokenied output back to sentences for sentimental analysis
+    for tweets_tokens in total_tweets_tokens:
+        tweet = ' '.join(words for words in tweets_tokens)
+        tweet_token_total.append(tweet)
+
+    for tweets_stems in total_stemmer_tokens:
+        tweets = ' '.join(words for words in tweets_stems)
+        tweet_stem_total.append(tweets)
+    return tweet_token_total, tweet_stem_total
+
+
+def score_value(score):
+    if score >= -0.2 and score < 0.2:
+        # print('Neutral')
+        output = 'Neutral'
+
+    elif score >= 0.20 and score < 0.60:
+        # print('Slightly Positive')
+        output = 'Slightly Positive'
+
+    elif score >= 0.60 and score <= 1.0:
+        # print('Positive')
+        output = 'Positive'
+
+    elif score >= -0.60 and score < -0.20:
+        # print('Slighly Negative')
+        output = 'Slighly Negative'
+
+    elif score >= -1.0 and score < -0.60:
+        # print('Negative')
+        output = 'Negative'
+
+    return output
+
+
+
+output_textblob = []
+output_vader = []
+
+
+for i, j in file.iterrows():
+
+    ################################ TEXTBLOB USING Tokenizing and stemming #####################
+    processed_tweets = tokens_stemmer(processing_text(j["tweet"]))
+    # print(processed_tweets[0])
+    tweet_stemmed = processed_tweets[1]
+    # print(tweet_stemmed)
+
+    for tweet in tweet_stemmed:
+        analyze = TextBlob(tweet)
+        text_blob_polarity = analyze.sentiment.polarity
+        # print(polarity)
+        print(tweet)
+        # writer.writerows(polarity)
+        output_textblob.append(score_value(text_blob_polarity))
+
+
+    ######################################## VADER SENTIMENT ANALYSIS ################################
+
+    analyzer = SentimentIntensityAnalyzer()
+    vader_sentimets = analyzer.polarity_scores(j["tweet"])
+    vader_compound_score = vader_sentimets['compound']
+
+    output_vader.append(score_value(vader_compound_score))
+
+
+
+
+
+file['output_textblob'] = output_textblob
+file['output_Vader']    = output_vader
+file1_save = file.to_excel("output_textblob.xlsx", "w")
+print("Output_textblob file saved successfully!!")
+
